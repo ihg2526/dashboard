590256ca24adcca9a804e358292dd9279f1091dd
xtylab <b10902010@csie.ntu.edu.tw>
2026-01-24 12:39:08 +0800

    refactor: Update Vite base path configuration to use `mode` instead of `command`.

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig(({ mode }) => ({
     plugins: [react()],
-    base: command === 'build' ? '/dashboard/' : '/',
+    base: mode === 'production' ? '/dashboard/' : '/',
     server: {
         port: 3000,
         proxy: {
